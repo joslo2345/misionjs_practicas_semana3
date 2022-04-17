d28e68ad397181ac27733046501cbd16468a1a08
joslo2345 <soulsilversx@gmail.com>
2022-04-17 16:55:46 -0500

    se agrega la practica 1 my_launch_app

---
+++ my_launchx_app/index.js
@@ -0,0 +1,11 @@
+const MissionCommander = require('./app/missionCommander');
+
+
+const carlo = new MissionCommander("Carlo");
+console.log(carlo.name);
+
+const dio = new MissionCommander("Dio");
+console.log(dio.name);
+
+const geno = new MissionCommander("Geno");
+console.log(geno.name);
