336d1be98ce8f17f5a4e6b3d363b900fde009fca
joslo2345 <soulsilversx@gmail.com>
2022-04-18 00:43:12 -0500

    se agrega practica 5 express_api resuelta

---
+++ express_api/app.js
@@ -0,0 +1,54 @@
+// Usando objeto express
+const express = require('express')
+// App de Express
+const app = express()
+app.use(express.json()) // Indicamos que usaremos JSON
+// Puerto en que vamos a ver nuestra app: localhost:3000
+const port = 3000
+
+// HTTP METHODS
+app.get('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`);
+    const explorer1 = {id:1,name:'carlo1'};
+    const explorer2 = {id:2,name:'carlo2'};
+    const explorer3 = {id:3,name:'carlo3'};
+    const explorer4 = {id:4,name:'carlo4'};
+    const explorer5 = {id:5,name:'carlo5'};
+    const explorers = [explorer1,explorer2,explorer3,explorer4,explorer5];
+    res.status(200).json(explorers);
+})
+
+app.get('/v1/explorers/:id' ,(req,res) => {
+    console.log(`Api Explorers GET request ${new Date()}`);
+    console.log(`Getting explorer with id ${req.params.id}`);
+    const explorer = {id:req.params.id,name:carlo};
+    res.status(200).json(explorer);
+})
+
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`);
+    const requestBody = req.body // Parametros de un cliente
+    res.status(201).json({message: "created"});
+})
+
+app.put('/v1/explorers:id', (req,res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`);
+    console.log(`Update explorer with id ${req.params.id}`);
+    const requestBody = req.body // Parametros de un cliente
+    res.status(200).json({message: "updated!"});
+})
+
+app.delete('/v1/explorers:id', (req,res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`);
+    console.log(`Delete explorer with id ${req.params.id}`);
+    const requestBody = req.body // Parametros de un cliente
+    res.status(200).json({message: "Deleted!"});
+})
+
+
+
+
+// Con esto inicializamos esta app
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
